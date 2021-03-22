f96b138b1b7319cbbae8eb5d7da80849a97613fe
jcolleran01 <joshuacolleran@hotmail.com>
2021-03-22 14:33:02 -0700

    made some cambios

--- app/src/main/java/com/example/criminalintent/Crime.java
+++ app/src/main/java/com/example/criminalintent/Crime.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
-
+    //figured out how to clone, commit and push
     private UUID mId;
     private String mTitle;
     private Date mDate;
