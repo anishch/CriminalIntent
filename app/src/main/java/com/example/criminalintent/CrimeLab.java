2e9105320a6a9cbe2f38e3cf5266dda77d6b8f81
anishch <43155397+anishch@users.noreply.github.com>
2021-03-27 19:31:05 -0700

    Customized Crime Title [Chapter 9]

--- app/src/main/java/com/example/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/criminalintent/CrimeLab.java
@@ -22,7 +22,7 @@ public class CrimeLab {
         mCrimes = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
+            crime.setTitle("COVID Violation #" + i);
             crime.setSolved(i % 2 == 0); // Every other one
             mCrimes.add(crime);
         }
