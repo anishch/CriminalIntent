3651ef340f725d94a561a388ac006bc3ae4c61ee
vpritamani <44382864+vpritamani@users.noreply.github.com>
2021-05-27 09:29:41 -0700

    Revert "Test unrelated to criminal intent"

--- app/src/main/java/com/example/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/example/criminalintent/CrimeListActivity.java
@@ -2,9 +2,9 @@ package com.example.criminalintent;
 
 import androidx.fragment.app.Fragment;
 
-public class ScoreListActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
-        return new ScoreListFragment();
+        return new CrimeListFragment();
     }
 }
